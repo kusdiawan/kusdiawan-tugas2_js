5f7e58c5d7c7a5501166d7bdb89eb6ea66a42f1b
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-09-18 10:39:44 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,14 @@
+var nim = "2020001";
+var nama = "Wawan Kusdiawan";
+var status = true;
+var nilai = null;
+var usia;
+var hobbi = ["jalan, sepeda, makan, nonton, mancing"];
+
+
+console.log("NIM : " + nim);
+console.log("Nama : " + nama);
+console.log("Status : " + status);
+console.log("Nilai : " + nilai);
+console.log("Usia : " + usia);
+console.log("Hobiku : " + hobbi);
